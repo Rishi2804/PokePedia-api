ba2f070f64a939f7a886f7d30551b3a7783f5af6
Rishi2804 <77213736+Rishi2804@users.noreply.github.com>
2024-10-01 16:34:53 -0400

    feat: created interface and implemenetation files for services and repositories

--- src/main/java/com/rishi/PokePedia/repository/PokemonRepository.java
+++ src/main/java/com/rishi/PokePedia/repository/PokemonRepository.java
@@ -1,137 +1,16 @@
 package com.rishi.PokePedia.repository;
 
-import java.util.List;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rishi.PokePedia.model.*;
-import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Repository;
 
-import java.sql.Array;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.util.List;
 import java.util.Optional;
 
-@Repository
-public class PokemonRepository {
+public interface PokemonRepository {
+    // Singular Pokemon Methods
+    Optional<Pokemon> getPokemonById(Integer id);
+    List<DexNumbers> getDexNumbersFromPokemon(Integer id);
+    List<EvolutionLine> getEvolutionChainOfPokemon(Integer id);
 
-    private final JdbcTemplate jdbcTemplate;
-    private static final ObjectMapper objectMapper = new ObjectMapper();
-
-
-    public PokemonRepository(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
-
-    public Optional<Pokemon> getPokemonById(Integer id) {
-        String sql = "SELECT * FROM pokemon WHERE id = ?";
-        try {
-            return Optional.ofNullable(jdbcTemplate.queryForObject(sql, PokemonRepository::pokemonRowMapper, id));
-        } catch (EmptyResultDataAccessException e) {
-            return Optional.empty();
-        }
-    }
-
-    public List<DexNumbers> getDexNumbersFromPokemon(Integer id) {
-        String sql = "SELECT * FROM dexnumber WHERE default_variate = ? OR alt_variates @> ARRAY[?]";
-        return jdbcTemplate.query(sql, PokemonRepository::dexNumberRowMapper, id, id);
-    }
-
-    public List<EvolutionLine> getEvolutionChainOfPokemon(Integer id) {
-        String sql = "SELECT * FROM get_evolution_chain_by_id(?)";
-        return jdbcTemplate.query(sql, PokemonRepository::evolutionRowMapper, id);
-    }
-
-    public List<PokemonDexSnap> getDexByRegion(PokedexRegion pokedexRegion) {
-        String sql = "";
-        if (pokedexRegion == PokedexRegion.NATIONAL) {
-            sql = "SELECT s.id AS num, p.id, s.name, p.type1, p.type2 FROM pokemon p " +
-                    "JOIN species s ON s.id = p.species_id " +
-                    "ORDER BY s.id, p.id ASC";
-            return jdbcTemplate.query(sql, PokemonRepository::pokedexRowMapper);
-        } else {
-            sql = "SELECT d.num, p.id, s.name, " +
-                    "COALESCE(pt.type1, p.type1) AS type1, " +
-                    "CASE WHEN pt.type1 IS NOT NULL THEN pt.type2 ELSE p.type2 END AS type2 " +
-                    "FROM dexnumber d " +
-                    "JOIN pokemon p ON d.default_variate = p.id OR d.alt_variates @> ARRAY[p.id] " +
-                    "JOIN species s ON s.id = d.species_id " +
-                    "LEFT JOIN pasttypes pt ON p.id = pt.pokemon_id AND pt.gen >= ? " +
-                    "WHERE d.name::text = ? " +
-                    "ORDER BY d.num, CASE WHEN p.id = d.default_variate THEN 0 ELSE 1 END";
-            return jdbcTemplate.query(sql, PokemonRepository::pokedexRowMapper, pokedexRegion.getGen(), pokedexRegion.getName());
-        }
-    }
-
-    private static Pokemon pokemonRowMapper(ResultSet rs, Integer rowNum) throws SQLException {
-        String type2Str = rs.getString("type2");
-        Type type2 = type2Str == null ? null : Type.fromString(type2Str);
-
-        Array formsArray = rs.getArray("forms");
-        String[] forms = formsArray == null ? null : (String[]) formsArray.getArray();
-
-        String[] dexEntriesJsonStrs = (String[]) rs.getArray("dex_entries").getArray();
-        Pokemon.DexEntry[] dexEntries = new Pokemon.DexEntry[dexEntriesJsonStrs.length];
-        for (int i = 0; i < dexEntriesJsonStrs.length; i++) {
-            try {
-                dexEntries[i] = objectMapper.readValue(dexEntriesJsonStrs[i], Pokemon.DexEntry.class);
-            } catch (Exception e) {
-                e.printStackTrace();
-                dexEntries[i] = null;
-            }
-        }
-
-        return new Pokemon(
-                rs.getInt("id"),
-                rs.getInt("species_id"),
-                rs.getString("name"),
-                rs.getInt("gen"),
-                Type.fromString(rs.getString("type1")),
-                type2,
-                rs.getFloat("weight"),
-                rs.getFloat("height"),
-                rs.getInt("gender_rate"),
-                rs.getInt("hp"),
-                rs.getInt("atk"),
-                rs.getInt("def"),
-                rs.getInt("spatk"),
-                rs.getInt("spdef"),
-                rs.getInt("speed"),
-                rs.getInt("bst"),
-                forms,
-                dexEntries
-        );
-    }
-
-    private static DexNumbers dexNumberRowMapper(ResultSet rs, Integer rowNum) throws SQLException {
-        return new DexNumbers(
-                rs.getInt("num"),
-                PokedexRegion.fromName(rs.getString("name"))
-        );
-    }
-
-    private static PokemonDexSnap pokedexRowMapper(ResultSet rs, Integer rowNum) throws SQLException {
-        String type2Str = rs.getString("type2");
-        Type type2 = type2Str == null ? null : Type.fromString(type2Str);
-        return new PokemonDexSnap(
-                rs.getInt("num"),
-                rs.getInt("id"),
-                rs.getString("name"),
-                Type.fromString(rs.getString("type1")),
-                type2
-        );
-    }
-
-    private static EvolutionLine evolutionRowMapper(ResultSet rs, Integer rowNum) throws SQLException {
-        return new EvolutionLine(
-                rs.getInt("id"),
-                rs.getInt("from_pokemon"),
-                rs.getString("from_display"),
-                rs.getInt("to_pokemon"),
-                rs.getString("to_display"),
-                (String[]) rs.getArray("details").getArray(),
-                rs.getString("region"),
-                rs.getInt("alt_form")
-        );
-    }
+    // Pokedex List
+    List<PokemonDexSnap> getDexByRegion(PokedexRegion pokedexRegion);
 }
--- src/main/java/com/rishi/PokePedia/service/PokemonService.java
+++ src/main/java/com/rishi/PokePedia/service/PokemonService.java
@@ -2,82 +2,11 @@ package com.rishi.PokePedia.service;
 
 import com.rishi.PokePedia.dto.PokemonDexSnapDto;
 import com.rishi.PokePedia.dto.PokemonDto;
-import com.rishi.PokePedia.model.*;
-import com.rishi.PokePedia.repository.PokemonRepository;
-import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
-@Service
-public class PokemonService {
-    private final PokemonRepository pokemonRepository;
-
-    public PokemonService(PokemonRepository pokemonRepository) {
-        this.pokemonRepository = pokemonRepository;
-    }
-
-    public Optional<PokemonDto> getPokemonById(Integer id) {
-        Optional<Pokemon> pokemon = pokemonRepository.getPokemonById(id);
-        List<DexNumbers> dexNumbers = pokemonRepository.getDexNumbersFromPokemon(id);
-        List<EvolutionLine> evolutionChain = pokemonRepository.getEvolutionChainOfPokemon(id);
-        return pokemon.map(value -> mapToPokemonDto(value, dexNumbers, evolutionChain));
-    }
-
-    public List<PokemonDexSnapDto> getDexByRegion(String name) {
-        PokedexRegion region = PokedexRegion.fromName(name);
-        return mapToPokedexDto(pokemonRepository.getDexByRegion(region));
-    }
-
-    private PokemonDto mapToPokemonDto(Pokemon pokemon, List<DexNumbers> dexNumbers, List<EvolutionLine> evolutionChain) {
-        return new PokemonDto(
-                pokemon.id(),
-                pokemon.speciesId(),
-                pokemon.name(),
-                pokemon.gen(),
-                pokemon.type1().getName(),
-                pokemon.type2() == null ? null : pokemon.type2().getName(),
-                pokemon.weight(),
-                pokemon.height(),
-                pokemon.genderRate(),
-                new PokemonDto.StatsDto(
-                        pokemon.hp(),
-                        pokemon.atk(),
-                        pokemon.def(),
-                        pokemon.spatk(),
-                        pokemon.spdef(),
-                        pokemon.speed(),
-                        pokemon.bst()
-                ),
-                pokemon.forms(),
-                Arrays.stream(pokemon.dexEntries())
-                        .map(dexEntry -> new PokemonDto.DexEntryDto(dexEntry.game(), dexEntry.entry()))
-                        .toArray(PokemonDto.DexEntryDto[]::new),
-                dexNumbers.stream()
-                        .map(dex -> new PokemonDto.DexNumberDto(dex.region().getName(), dex.dexNumber()))
-                        .toArray(PokemonDto.DexNumberDto[]::new),
-                evolutionChain.stream()
-                        .map(line -> new PokemonDto.EvolutionLineDto(
-                                line.id(),
-                                line.fromPokemon(),
-                                line.fromDisplay(),
-                                line.toPokemon(),
-                                line.toDisplay(),
-                                line.details(),
-                                line.region(),
-                                line.altForm()
-                        ) ).toArray(PokemonDto.EvolutionLineDto[]::new)
-        );
-    }
-
-    private List<PokemonDexSnapDto> mapToPokedexDto(List<PokemonDexSnap> dexByRegion) {
-        return dexByRegion.stream().map(entry -> new PokemonDexSnapDto(
-                entry.dexNumber(),
-                entry.id(),
-                entry.name(),
-                entry.type1().getName(),
-                entry.type2() == null ? null : entry.type2().getName()
-        )).toList();
-    }
+public interface PokemonService {
+    Optional<PokemonDto> getPokemonById(Integer id);
+    List<PokemonDexSnapDto> getDexByRegion(String name);
 }
